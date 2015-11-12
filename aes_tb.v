f91eddbd4de8125ab5b8998b29489c36e999470c
meenuh <dearmeenuh@gmail.com>
2015-11-11 20:17:30 -0800

    adding actual test

--- aes_tb.v
+++ aes_tb.v
@@ -26,14 +26,18 @@ module aes_encrypt(
 		output wire [127:0] ciphertext);*/
 		
 module aes_tb;
-    reg [127*8:0] text; //to store the string to encrypt/decrypt
+    reg [127:0] text ; //to store the string to encrypt/decrypt
+    reg [127:0] key;
+    wire [127:0] ciphertext, dText;
     
-    
-    aes_encrypt(.plaintext(text), .key(key), .ciphertext(ciphertext));
+    aes_encrypt m1(.plaintext(text), .key(key), .ciphertext(ciphertext));
+    aes_decrypt m2(.plaintext(ciphertext), .key(key), .ciphertext(dText));
     initial begin
-        text = "Hello my name is Mina";
+        text = 48'b010010000110010101101100011011000110111100100001; //Hello!
+        key = 32'b01101011011001010111100100001010; //key
        
-       $display("%s", text);
-       
+       $display("%s %s", text, key);
+       $display("%s %s", ciphertext, dText);
+
     end
 endmodule
